94b4f3e49071b69f4b35af58319fb1be6bd4e71c
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-08-02 18:32:46 +0300

    show metho in Article contoller

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class ArticlesController extends Controller
 {
-    public function show($article) {
-
+    public function show($id) {
+        $article = Article::find($id);
+        return view('articles.show')->with('article', $article);
     }
 }
