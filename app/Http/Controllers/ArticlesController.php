f7c59a6d6b2b7bf84cd66dabd2c9901c6983e223
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-08-04 17:19:53 +0300

    create and update views

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -25,18 +25,24 @@ class ArticlesController extends Controller
     }
 
     // Persist the new resource
-    public function store() {
+    public function store(Request $request) {
+        $article = new Article();
 
+        $article->save();
+        return redirect('/articles');
     }
 
     // Show a view to edit an existing resource
-    public function edit() {
-
+    public function edit($id) {
+        $article = Article::find($id);
+        return view('articles.edit')->with('article', $article);
     }
 
     // Persist the edited resource
-    public function update() {
-
+    public function update($id) {
+        $article = Article::find($id);
+        $article->save();
+        return redirect('/articles'.$article->id);
     }
 
     // Delete the resource
--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -3,25 +3,26 @@
 @section('content')
     <div id="wrapper">
         <div id="page" class="container">
-            <h1 class="heading has-text-weight-bold is-size-4">New Article</h1>
-            <form method="post" action="/articles">
+            <h1 class="heading has-text-weight-bold is-size-4">Update Article</h1>
+            <form method="post" action="/articles/{{$article->id}}">
                 @csrf
+                @method('PUT')
                 <div class="field">
                     <label for="title" class="label">Title</label>
                     <div class="contol">
-                        <input type="text" class="input" name="title" id="title" />
+                        <input type="text" class="input" name="title" id="title" value="{{ $article->title }}" />
                     </div>
                 </div>
                 <div class="field">
                     <label for="excerpt" class="label">Excerpt</label>
                     <div class="control">
-                        <textarea class="textarea" name="excerpt" id="excerpt" cols="30" rows="10"></textarea>
+                        <textarea class="textarea" name="excerpt" id="excerpt" cols="30" rows="10">{{ $article->excerpt }}</textarea>
                     </div>
                 </div>
                 <div class="field">
                     <label for="body" class="label">Body</label>
                     <div class="control">
-                        <textarea class="textarea" name="body" id="body" cols="30" rows="10"></textarea>
+                        <textarea class="textarea" name="body" id="body" cols="30" rows="10">{{ $article->body }}</textarea>
                     </div>
                 </div>
                 <div class="field is-grouped">
