f97353ed1e2d71740b85996d8c6cf0e279a3bc3b
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-08-05 16:50:41 +0300

    article show page

---
+++ resources/views/articles/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layout')
+
+@section('content')
+    <div id="wrapper">
+        <div id="page" class="container">
+            <div id="content">
+                <div class="title">
+                    <h2>{{ $article->title }}</h2>
+                </div>
+                <p>
+                    <img src="{{ asset('image/banner.jpg') }}" alt="" class="image image_full">
+                </p>
+                {!! $article->body !!}
+                <p>
+                    @foreach($article->tags as $tag)
+                        <a href="#">{{ $tag->name }}</a>
+                    @endforeach
+                </p>
+            </div>
+        </div>
+    </div>
+@endsection
