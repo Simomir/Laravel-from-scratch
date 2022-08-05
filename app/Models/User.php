1770e6c291cf9129bcba8a6968b43faa50f6a05d
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-08-05 16:08:41 +0300

    author method in article model

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,8 +45,4 @@ class User extends Authenticatable
     public function articles() {
         return $this->hasMany(Article::class);
     }
-
-    public function projects() {
-        return $this->hasMany(Project::class);
-    }
 }
